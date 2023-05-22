6dd58c9481f3d0eaec99272c6dd60b799547defe
MasterPC <free_person@inbox.ru>
2023-05-22 14:24:34 +0300

    0. Spring Security [#6879]

---
+++ src/main/java/ru/job4j/accidents/filter/SecurityInit.java
@@ -0,0 +1,7 @@
+package ru.job4j.accidents.filter;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+
+}
